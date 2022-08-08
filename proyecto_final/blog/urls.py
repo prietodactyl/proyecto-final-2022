85d678c4f19ecf4a303aad06e95bee13eb7a5243
AnaM0411 <anitamazal@hotmail.com>
2022-08-07 21:50:12 -0300

    agregué barras a las urls

--- proyecto_final/blog/urls.py
+++ proyecto_final/blog/urls.py
@@ -5,10 +5,10 @@ from . import views
 
 urlpatterns = [
     path('', views.inicio, name='inicio'),
-    path('quienes_somos', views.quienes_somos, name='quienes_somos'),
-    path('noticias', views.noticias, name='noticias'),
-    path('noticias/crear', views.crear_noticia, name='crear_noticia'),
-    path('noticias/editar', views.editar_noticia, name='editar_noticia'),
-    path('descargas', views.descargas, name='descargas'),
-    path('contacto', views.contacto, name='contacto')
+    path('quienes_somos/', views.quienes_somos, name='quienes_somos'),
+    path('noticias/', views.noticias, name='noticias'),
+    path('noticias/crear/', views.crear_noticia, name='crear_noticia'),
+    path('noticias/editar/', views.editar_noticia, name='editar_noticia'),
+    path('descargas/', views.descargas, name='descargas'),
+    path('contacto/', views.contacto, name='contacto')
 ]
