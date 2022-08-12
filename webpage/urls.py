4286aaeb48f6ebddeffe82537b0c0df6df7fbea0
Prietodactyl <agus.prieto.93@gmail.com>
2022-08-11 23:25:07 -0300

    prieto

--- webpage/urls.py
+++ webpage/urls.py
@@ -14,8 +14,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('main.urls')),
+    path('noticias', include('noticias.urls')),
+    path('nosotros', include('nosotros.urls')),
+    path('descargas', include('descargas.urls')),
+    path('contacto', include('contacto.urls')),
 ]
