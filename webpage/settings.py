0e617c19f6f152338de24d042c39c42f74840e9f
Prietodactyl <agus.prieto.93@gmail.com>
2022-08-16 21:02:27 -0300

    prieto

--- webpage/settings.py
+++ webpage/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -121,8 +122,17 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, "static"),
+)
+
+print("***************************************")
+print(os.path.join(BASE_DIR, "static"))
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
+
+MEDIA_ROOT = os.path.join(BASE_DIR, '')
+MEDIA_URL = '/assets/pictures/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
