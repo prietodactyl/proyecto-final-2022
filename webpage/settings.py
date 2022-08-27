deaf5e1b09d1bf51943449c81ce895122ec24220
SantiagoPagno <110251262+SantiagoPagno@users.noreply.github.com>
2022-08-27 16:00:49 -0300

    agregue archivo requirements.tx

--- webpage/settings.py
+++ webpage/settings.py
@@ -81,10 +81,18 @@ WSGI_APPLICATION = 'webpage.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'mssql',
+        'NAME': 'proyecto-final-2022',
+        'USER':'sa',
+        'PASSWORD':'santi123',
+        'HOST':'DESKTOP-HAP4VG2\SQLEXPRESS',
+        'PORT': '',
+        'OPTIONS': {
+            'DRIVER':'ODBC Driver 17 for SQL Server',
+        }
     }
 }
+
 
 
 # Password validation
